b52daa638f20d234b46fa271aeba86d6cc3911c0
basnopisets <oleg.s.osipenko@gmail.com>
2015-03-18 04:19:42 +0300

    рабочий макет

--- app/src/main/java/oleg/osipenko/lentarssreader/list/RssAdapter.java
+++ app/src/main/java/oleg/osipenko/lentarssreader/list/RssAdapter.java
@@ -1,10 +1,15 @@
 package oleg.osipenko.lentarssreader.list;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,6 +17,7 @@ import java.util.TreeMap;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import oleg.osipenko.lentarssreader.R;
 import oleg.osipenko.lentarssreader.rss_model.Item;
 
 /**
@@ -20,25 +26,46 @@ import oleg.osipenko.lentarssreader.rss_model.Item;
 public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
 
     private ArrayList<Item> mValues;
-    private ItemClickListener mItemClickListener;
 
-    public RssAdapter(ItemClickListener itemClickListener) {
-
+    public RssAdapter() {
         mValues = new ArrayList<>();
-        mItemClickListener = itemClickListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(android.R.layout.simple_list_item_1, parent, false);
+                .inflate(R.layout.item_feed, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTextView.setText(mValues.get(position).getDate().toString());
-        holder.setOnClickListener((View v) -> mItemClickListener.itemClicked(position));
+        Item item = mValues.get(position);
+        holder.mTvTitle.setText(item.getTitle());
+        holder.mTvDescription.setText(item.getDescription());
+        if (!TextUtils.isEmpty(item.getImageUrl())) {
+            holder.mIvFeed.setVisibility(View.VISIBLE);
+            Picasso.with(holder.getContext())
+                    .load(item.getImageUrl())
+                    .into(holder.mIvFeed);
+        } else {
+            holder.mIvFeed.setImageResource(0);
+            holder.mIvFeed.setVisibility(View.GONE);
+        }
+        holder.mTvSource.setText(item.getSource());
+        if (item.isExpanded()) {
+            holder.mTvDescription.setVisibility(View.VISIBLE);
+        } else {
+            holder.mTvDescription.setVisibility(View.GONE);
+        }
+        holder.getParent().setOnClickListener((View v) -> {
+            if (item.isExpanded()) {
+                item.setExpanded(false);
+            } else {
+                item.setExpanded(true);
+            }
+            notifyItemChanged(position);
+        });
     }
 
     @Override
@@ -53,8 +80,14 @@ public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
-        @InjectView(android.R.id.text1)
-        TextView mTextView;
+        @InjectView(R.id.iv_feed)
+        ImageView mIvFeed;
+        @InjectView(R.id.tv_title)
+        TextView mTvTitle;
+        @InjectView(R.id.tv_description)
+        TextView mTvDescription;
+        @InjectView(R.id.tv_source)
+        TextView mTvSource;
 
         private final View mParent;
 
@@ -64,12 +97,12 @@ public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
             ButterKnife.inject(this, itemView);
         }
 
-        public void setOnClickListener(View.OnClickListener listener) {
-            mParent.setOnClickListener(listener);
+        public Context getContext() {
+            return mParent.getContext();
         }
-    }
 
-    public interface ItemClickListener {
-        public void itemClicked(int position);
+        public View getParent() {
+            return mParent;
+        }
     }
 }
